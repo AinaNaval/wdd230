3cc3cf4358cfff6d4282a1154e7cac34e98c4bc2
AinaNaval <ainanaval22@gmail.com>
2023-11-15 12:38:34 +0300

    week2

---
+++ scripts/dark.js
@@ -0,0 +1,21 @@
+const modeButton = document.querySelector("#mode");
+const section1 = document.querySelector(".card1");
+const section2 = document.querySelector(".card2");
+const body = document.querySelector("body");
+const h2 = document.querySelector("h2");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+        body.style.background = "#000";
+		section1.style.background = "#000";
+        section2.style.background = "#000";
+		modeButton.textContent = "🔆";
+        h2.style.color = "#fff";
+	} else {
+        body.style.background = "#fff";
+		section1.style.background = "cadetblue";
+		section2.style.background = "cadetblue";
+		modeButton.textContent = "🕶️";
+        h2.style.color = "#000";
+	}
+});
