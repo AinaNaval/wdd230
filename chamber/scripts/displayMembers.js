7d89b4984ec964bd9779bfec9003daca84f2bb89
AinaNaval <ainanaval22@gmail.com>
2023-12-03 12:05:43 +0300

    new

--- chamber/scripts/displayMembers.js
+++ chamber/scripts/displayMembers.js
@@ -3,7 +3,38 @@ const listbutton = document.querySelector("#list");
 const display = document.querySelector("article");
 
 const members = document.querySelector(".members");
-const url = "https://ainanaval.github.io/wdd230/chamber/data/displayMembers.js";
+const url = "https://ainanaval.github.io/wdd230/chamber/data/members.json";
+
+async function getMembers() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    displayMembers(data.members);
+}
+
+getMembers();
+
+function displayMembers(members) {
+    members.forEach((member) => {
+        let newImg = document.createElement("img");
+        let newH3 = document.createElement("h3");
+        let newP = document.createElement("p");
+        let newSection = document.createElement("section");
+        let newDiv = document.createElement("div");
+        display.appendChild(newSection);
+
+        newH3.textContent = member.name;
+        newP.textContent = `Membership Level: ${member.membershiplevel}`
+        newImg.setAttribute('src', member.photo);
+        newImg.setAttribute('src', `${member.name}'s photo`);
+
+        newSection.appendChild(newDiv);
+        newDiv.appendChild(newImg);
+        newSection.appendChild(newH3);
+        newSection.appendChild(newP);
+    });
+}
+
 
 // The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 
