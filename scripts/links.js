84556c1b537a17f25bd78d44a0d90f2c349b3097
AinaNaval <ainanaval22@gmail.com>
2023-12-03 07:04:32 +0300

    next

--- scripts/links.js
+++ scripts/links.js
@@ -17,17 +17,17 @@ function displayLinks(weeks) {
         let liLength = (Object.keys(week.links).length)
         let i = 0;
         container.appendChild(newList);
-        console.log(week.week)
+        let listContent = `${week.week}: `;
+        newList.innerHTML += listContent;
+        //console.log(week.week)
 
         while (i < liLength) {
             let newLink = document.createElement('a');
             newLink.setAttribute('href',week.links[i].url);
-            newLink.textContent = week.links[i].title;
+            newLink.textContent = week.links[i].title + "| ";
             // console.log('url: ' + week.links[i].url + 'title: ' + week.links[i].title)
             newList.appendChild(newLink);
-            console.log(newLink)
             i++;
-        }
-
+        } 
     });
 }
